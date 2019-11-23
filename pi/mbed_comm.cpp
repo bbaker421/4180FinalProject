a49896c8deca404b49f51e7db02fc8e1f5ea503b
apurva-deshmukh <aadeshmukh13@gmail.com>
2019-11-22 21:54:48 -0500

    Update mbed communication

--- pi/mbed_comm.cpp
+++ pi/mbed_comm.cpp
@@ -28,15 +28,17 @@ int main(int argc, char ** argv) {
   options.c_cflag |= CREAD;
   cfmakeraw(&options);
   tcsetattr(fd,TCSANOW,&options);    //Set serial to new settings
-  sleep(1);
+  //sleep(1);
   // Write to the port
-  n = write(fd,"Hello mbed\r",11);
-  if (n < 0) {
+
+  //n = write(fd,"Sending result to mbed...\r",26);
+  
+  /*if (n < 0) {
     perror("Write failed - ");
     return -1;
-  }
+  }*/
   // Read the characters from the port if they are there
-  sleep(2);
+  //sleep(2);
   n = read(fd, buf, 11);
   if (n < 0) {
     perror("Read failed - ");
@@ -46,7 +48,7 @@ int main(int argc, char ** argv) {
     buf[n] = 0;
     printf("%i bytes read back: %s\n\r", n, buf);
   }
-  sleep(1);
+  //sleep(1);
   //Send command to blink mbed led 10 times at one second rate
   //mbed code turns on led2 with a '1' and off with a '0'
   //mbed echoes back each character
@@ -68,4 +70,3 @@ int main(int argc, char ** argv) {
   close(fd);
   return 0;
 }
- 
