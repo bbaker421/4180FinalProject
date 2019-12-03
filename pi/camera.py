d451e26d0d306ff9ee8eacaebf7dd401befc3207
apurva-deshmukh <aadeshmukh13@gmail.com>
2019-12-03 17:38:33 -0500

    take picture on button press

--- pi/camera.py
+++ pi/camera.py
@@ -1,37 +1,39 @@
 from picamera import PiCamera
 from time import sleep
 import os
+import RPi.GPIO as GPIO
 
-"""
-import RPI.GPIO as GPIO
-
-GPIO.setWarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO_PUD_DOWN)
-
-while (True):
-   if GPIO.input(10) == GPIO.HIGH:
-        print("1");
-
-"""
 camera = PiCamera()
+def button_callback(channel):
+    camera.capture('/home/pi/Desktop/unknown_image.jpg')
+    camera.stop_preview()
+    import requests
+
+    url = 'http://143.215.98.174:5000/authenticate'
+    files = {'file': open('/home/pi/Desktop/unknown_image.jpg', 'rb')}
+    r = requests.post(url, files=files)
+
+    print(r.content)
+
+    cmd = './mbed_comm ' + r.content.decode("utf-8")
+    os.system(cmd)
+    camera.start_preview()
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 camera.start_preview()
-sleep(1)
-camera.capture('/home/pi/Desktop/unknown_image.jpg')
-camera.stop_preview()
-camera.close()
+GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 
-import requests
+message = input("Press enter to quit\n\n")
+while(1):
+    pass
+GPIO.cleanup()
 
-url = 'http://10.0.0.236:5000/authenticate'
-files = {'file': open('/home/pi/Desktop/unknown_image.jpg', 'rb')}
-r = requests.post(url, files=files)
 
-print(r.content)
 
-cmd = './mbed_comm ' + r.content.decode("utf-8")
-os.system(cmd)
 
 
 
