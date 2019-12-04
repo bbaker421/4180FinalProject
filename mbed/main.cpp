1f07102c143c2f848d1aabef37167452a5d32fb9
Daniel Greene <daniel.greene@gatech.edu>
2019-12-04 14:07:30 -0500

    mbed code update

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "Servo.h"
+#include "Speaker.h"
 //Class to control an RGB LED using three PWM pins
 class RGBLed
 {
@@ -29,9 +30,13 @@ void RGBLed::write(float red,float green, float blue)
  
  
 //Setup RGB led using PWM pins and class
+DigitalOut myled(LED1);
+
 RGBLed myRGBled(p24,p25,p26); //RGB PWM pins
  
 Servo s(p22);
+
+PwmOut speaker(p21);
 
 RawSerial  pi(USBTX, USBRX);
 
@@ -41,36 +46,52 @@ void dev_recv()
     while(pi.readable()) {
         temp = pi.getc();
         pi.putc(temp);
-        if (temp=='1') {
+        if (temp == '1') 
+        {
+            
             myRGBled.write(0.0,1.0,0.0);
             s = 0.9; // Open
+        speaker.period(1.0/300.0); // 500hz period
+        speaker =0.50; //25% duty cycle - mid range volume
+        wait(.5);
+        speaker=0.0; // turn off audio
+    
             wait(3.0);
             s = 0.0; // Close
-            }
-        if (temp=='0')  myRGBled.write(1.0,0.0,0.0);
+            NVIC_SystemReset();
+            
+        }
+        if (temp == '0') 
+        {
+            myRGBled.write(1.0,0.0,0.0);
+            for (int i=0; i<7; i=i+2) {
+            speaker.period(1.0/969.0);
+            speaker = float(i)/50.0;
+            wait(.5);
+            speaker.period(1.0/800.0);
+            wait(.5);
+    }
+    speaker = 0.0;
+    NVIC_SystemReset();
+    
+        }
+        myled = 0;
+        
     }
 }
 
 int main()
 {
-     s.calibrate(0.001, 45.0);
-     s = 0.0;
-     pi.baud(9600);
+    
+    speaker = 0.0;
+    myled = 1;
+    s.calibrate(0.001, 45.0);
+    s = 0.0;
+    pi.baud(9600);
     pi.attach(&dev_recv, Serial::RxIrq);
     while(1) {
         sleep();
     }
     
-   /* while(1) {
-        if (pc.getc() == '1'){
-             myRGBled.write(0.0,1.0,0.0);
-             while (1){}
-        } else if (pc.getc() == '0') {
-            myRGBled.write(1.0,0.0,0.0);
-             while (1){}
-        } else {
-            myRGBled.write(0.0,0.0,1.0);
-            }
-    }*/
     
 }
